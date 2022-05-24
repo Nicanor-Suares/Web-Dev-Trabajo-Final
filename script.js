fe8ba4639e58e17406c214b1e4d51873816f29de
Nica <nicanorsuaressanchez@gmail.com>
2022-05-24 15:55:10 -0300

    Updated Blog Page / Minor Fixes
    Updated Blog Page:
    - Added Grid for Articles
    - Uploaded Images
    Minor Fixes:
    - Minor fixes in index page (blog section)
    - Added footer in every page

---
+++ script.js
@@ -0,0 +1,13 @@
+function validation(){
+
+    var name = document.forms["formulario"]["nombre"].value;
+    if(name == null || x ==""){
+
+        alert("Por favor, ingrese su nombre")
+        return false;
+
+    }
+
+}
+
+//add autofocus on error
